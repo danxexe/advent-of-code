5dc51966598acf0b0cc935415f10bff77f41a601
danxexe <danx.exe@gmail.com>
2023-12-07 16:33:49 -0300

    Change directory structure + template

---
+++ 2023/elixir/lib/Day00/Day00.ex
@@ -0,0 +1,50 @@
+defmodule Day00 do
+
+  @sample_data_part_1 """
+  """
+
+  @sample_data_part_2 @sample_data_part_1
+
+  use Aoc.SolutionUtils
+
+  @doc ~S"""
+  ## Examples
+
+      iex> __MODULE__.sample_solution_part1()
+      nil
+  """
+  def sample_solution_part1()
+
+  @doc ~S"""
+  ## Examples
+
+      iex> __MODULE__.solution_for_file_part1()
+      nil
+  """
+  def solution_for_file_part1()
+
+  @doc ~S"""
+  ## Examples
+
+      iex> __MODULE__.sample_solution_part2()
+      nil
+  """
+  def sample_solution_part2()
+
+  @doc ~S"""
+  ## Examples
+
+      iex> __MODULE__.solution_for_file_part2()
+      nil
+  """
+  def solution_for_file_part2()
+
+
+  defp solution_part1(lines) do
+    lines
+  end
+
+  defp solution_part2(lines) do
+    lines
+  end
+end
